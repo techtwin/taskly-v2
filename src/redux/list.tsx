a7b17f8035d254a9e7807abf59d6f13dd084d8b1
Shevaughn <gshevaughn@gmail.com>
2021-07-01 13:42:01 -0400

    getting files ready with redux

---
+++ src/redux/list.tsx
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+const url: string = "http://localhost:3000/";
+
+const listSlice = createSlice({
+  name: "list",
+  initialState: { lists: [] },
+  reducers: {
+    
+  }
+})
